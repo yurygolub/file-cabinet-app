50da866a442d2247a46b2c3dd6a74b7f98edb086
yuragolub21@gmail.com <yuragolub21@gmail.com>
2021-09-26 23:07:12 +0300

    Add initial version of FileCabinetApp.

---
+++ FileCabinetApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FileCabinetApp
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
