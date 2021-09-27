67c0da35a2ce2096db586dad34011742ba6c69a9
yuragolub21@gmail.com <yuragolub21@gmail.com>
2021-09-27 23:55:37 +0300

    Add guard clauses to CreateRecord method.

--- FileCabinetApp/FileCabinetService.cs
+++ FileCabinetApp/FileCabinetService.cs
@@ -9,6 +9,46 @@ namespace FileCabinetApp
 
         public int CreateRecord(string firstName, string lastName, DateTime dateOfBirth, short weight, decimal account, char letter)
         {
+            if (string.IsNullOrWhiteSpace(firstName))
+            {
+                throw new ArgumentNullException(nameof(firstName));
+            }
+
+            if (firstName.Length < 2 || firstName.Length > 60)
+            {
+                throw new ArgumentException("firstName length is less than 2 or more than 60.");
+            }
+
+            if (string.IsNullOrWhiteSpace(lastName))
+            {
+                throw new ArgumentNullException(nameof(lastName));
+            }
+
+            if (lastName.Length < 2 || lastName.Length > 60)
+            {
+                throw new ArgumentException("lastName length is less than 2 or more than 60.");
+            }
+
+            if (dateOfBirth < new DateTime(1950, 1, 1) || dateOfBirth > DateTime.Now)
+            {
+                throw new ArgumentException("dateOfBirth is less than 01-Jan-1950 or more than now.");
+            }
+
+            if (weight < 1 || weight > 500)
+            {
+                throw new ArgumentException("weight is less than 1 or more than 500.");
+            }
+
+            if (account < 0)
+            {
+                throw new ArgumentException("account is less than zero.");
+            }
+
+            if (!char.IsLetter(letter))
+            {
+                throw new ArgumentException("letter is not a letter.");
+            }
+
             var record = new FileCabinetRecord
             {
                 Id = this.list.Count + 1,
