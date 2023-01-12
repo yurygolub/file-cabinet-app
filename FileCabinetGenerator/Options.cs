4a65d6203020aa7b9246db4ca217e1fffcb7980a
Yury Golub <yuragolub21@gmail.com>
2023-01-12 11:16:12 +0300

    Refactor command line parsing.

---
+++ FileCabinetGenerator/Options.cs
@@ -0,0 +1,19 @@
+﻿using CommandLine;
+
+namespace FileCabinetGenerator
+{
+    public class Options
+    {
+        [Option('t', "output-type", Required = true, HelpText = "Output file type")]
+        public string OutputType { get; set; }
+
+        [Option('o', "output", Required = true, HelpText = "Output file name")]
+        public string OutputFileName { get; set; }
+
+        [Option('a', "records-amount", Required = true, HelpText = "Records amount")]
+        public int RecordsAmount { get; set; }
+
+        [Option('i', "start-id", Required = true, HelpText = "Start id")]
+        public int StartId { get; set; }
+    }
+}
